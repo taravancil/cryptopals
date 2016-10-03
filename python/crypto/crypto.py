582f6ca12d580164e0661c05b9739eff76f865ba
Tara Vancil <tbvanc@gmail.com>
2016-10-03 16:29:13 -0400

    add crypto.crypto module

---
+++ python/crypto/crypto.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def xor(b1, b2):
+    """Return the result of XORing two equal-length bytes objects."""
+
+    if len(b1) != len(b2):
+        raise Exception('bytes objects must have the same length')
+
+    result = ''
+    for i in range(0, len(b1)):
+        result += b1[i] ^ b2[i]
+
+    return result
