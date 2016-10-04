46e0c40acf36ab41e900be029cd41ca880370734
Tara Vancil <tbvanc@gmail.com>
2016-10-04 16:00:06 -0400

    make utils python3 compatible

--- python/crypto/utils.py
+++ python/crypto/utils.py
@@ -1,6 +1,17 @@
-#!/usr/bin/python
-
+#!/usr/bin/python3
 """Utilities and wrappers for encoding and decoding."""
+
+import codecs
+
 def hex_to_base64(hexstr):
     """Convert the given string to base64."""
-    return hexstr.decode('hex').encode('base64').strip() # strip newline
+    decoded = codecs.decode(hexstr, 'hex')
+    return codecs.encode(decoded, encoding='base64')
+
+def hex_to_bytes(hexstr):
+    """Convert the given string to a bytes object."""
+    decoded = codecs.decode(hexstr, 'hex')
+    return str_to_bytes(decoded)
+
+def str_to_bytes(s):
+    return bytes(s)
