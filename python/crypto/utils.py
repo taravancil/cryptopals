a6f7e01ee4a9b41c41d0c4711c8e589067789db5
Tara Vancil <tbvanc@gmail.com>
2016-10-04 19:05:20 -0400

    add function to find most popular byte in bytes object

--- python/crypto/utils.py
+++ python/crypto/utils.py
@@ -22,3 +22,32 @@ def bytes_to_hex(_bytes):
 
 def str_to_bytes(s):
     return bytes(s)
+
+def get_popular_byte(_bytes):
+    """
+    Return the most popular byte in a bytes object. O(n) time when k
+    <= n. O(k) space.
+    space.
+    """
+
+    # Max value is 255, k = max + 1
+    k = 256
+
+    # Create a lookup array of length k to track the count of
+    # each byte. Bytes will be looked up by their index.
+    lookup = [0] * k
+
+    # O(n)
+    for b in _bytes:
+        lookup[b] += 1
+
+    max_count = 0
+    result = 0
+
+    # Iterate through the lookup array. O(k) == O(256)
+    for i in range(0, k):
+        if lookup[i] > max_count:
+            max_count = lookup[i]
+            result = i
+
+    return result
