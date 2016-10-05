e7c29a7802e331163d50e8c29413e0017605e36a
Tara Vancil <tbvanc@gmail.com>
2016-10-04 20:37:56 -0400

    rename

--- python/challenges.py
+++ python/challenges.py
@@ -72,7 +72,7 @@ def chal3():
     # do I figure this out with frequency analysis and without advance
     # knowledge of the fact that there are a bunch of NULL bytes?
     key = utils.get_popular_byte(ciphertext)
-    plaintext = crypto.xor(ciphertext, key)
+    plaintext = crypto.xor_single_byte_key(ciphertext, key)
 
     expect(plaintext.decode('utf-8'), OUT.decode('utf-8'))
 
@@ -94,7 +94,7 @@ def chal4():
         key = utils.get_popular_byte(ciphertext)
 
         # Try the key
-        plaintext_bytes = crypto.xor(ciphertext, key)
+        plaintext_bytes = crypto.xor_single_byte_key(ciphertext, key)
 
         score = utils.english_score(str(plaintext_bytes))
 
